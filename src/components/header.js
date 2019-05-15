ca5c924b35ecbe744c068bbc6271348a7da469a1
Paul <gladsomebeast@gmail.com>
2019-05-14 23:38:31 -0700

    New headshot.  Changed Copy.  Version update

--- src/components/header.js
+++ src/components/header.js
@@ -14,11 +14,7 @@ const Header = () => (
         }
         fileNamey: file(relativePath: { eq: "images/face.jpg" }) {
           childImageSharp {
-            fluid(maxWidth: 100, maxHeight: 100,
-              duotone: {
-                highlight: "#FFFFFF",
-                shadow: "#6666FF"
-              }) {
+            fluid(maxWidth: 100, maxHeight: 100) {
               ...GatsbyImageSharpFluid
             }
           }
